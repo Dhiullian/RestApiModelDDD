05de021ad91ea97c3b33bce6b215570e5b9f38f6
Dhiullian <dhiullian@donuz.co>
2021-05-08 18:56:52 -0300

    :rocket: starting project: api .net using DDD concepts.

---
+++ RestApiModelDDD.API.Domain/Entities/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RestApiModelDDD.Domain.Entities
+{
+    public class Client : Base
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime Datacadastro{ get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        public bool ativo { get; set; }
+    }
+}
